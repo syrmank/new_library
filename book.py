cc09fce1ded7552a9b98831d9a68a8e49b7f6c2e
student <student@ggpi.oirg>
2025-10-16 11:25:31 +0400

    Add __str__

--- book.py
+++ book.py
@@ -1,1 +1,11 @@
-print("asdf")
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    def __str__(self) -> str:
+        return f"'{self.title}', автор - {self.author}, год публикации - {self.year}"
+    
+book1 = Book("Название", "Автор", 1990)
+print(book1)
