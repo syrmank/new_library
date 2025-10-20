f70e531f7e15fb2bb1da8e68c344e2c2aea70050
Teacher <teacher@ggpi.org>
2025-10-20 11:03:52 +0400

    add def __str__

--- book.py
+++ book.py
@@ -1,12 +1,12 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title: str, author: str, year: int, isbn: str):
         self.title = title
         self.author = author
         self.year = year
-    
-    
+        self.isbn = isbn
+
     def __str__(self) -> str:
         return f"'{self.title}', автор - {self.author}, год публикации - {self.year}"
     
-book1 = Book("Название1", "Автор1", 1991)
+book1 = Book("Название1", "Автор1", 1991, "978-5-17-080077-7")
 print(book1)
