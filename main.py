7e433cc61b6c313b9e8fa3506529de1a9207c221
Teacher <teacher@ggpi.org>
2025-10-21 13:32:51 +0400

    Decomposition is done

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.models import Book
+
+book1 = Book("Название1", "Автор1", 1991, "978-5-17-080077-7")
+print(book1)
--- src/models/book.py
+++ src/models/book.py
@@ -7,6 +7,3 @@ class Book:
 
     def __str__(self) -> str:
         return f"'{self.title}', автор - {self.author}, год публикации - {self.year}"
-    
-book1 = Book("Название1", "Автор1", 1991, "978-5-17-080077-7")
-print(book1)
