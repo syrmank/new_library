6c520cc7538b44e19c3343c2b4a9fc6c27c734f8
Teacher <teacher@ggpi.org>
2025-11-25 13:39:41 +0400

    main.py: new book

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.models import Book
 
 title1 = "Название книги ну очень длинное, оооочень"
-book1 = Book(title1, "Автор1", 1991, "978-5-17-080077-7")
+author1 = "Автор1"
+
+
+book1 = Book(title1, author1, 1991, "978-5-17-080077-7")
+print(book1)
+book2 = Book(title2, author2, 1992, "978-5-17-080077-2")
 print(book1)
